5e86ccf5f24bcfab9a9875964b8611cba77efc5b
Daniel Longacre <danieljlongacre@gmail.com>
2024-05-08 20:28:15 -0400

    Added user register screen and register mutation

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use(express.static(path.join(__dirname, '/frontend/dist')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+// );
+
 // Cookie parser middleware
 app.use(cookieParser());
 
