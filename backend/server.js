1fb27985ff514bc0b31c2bff8a759fa288c31d28
Daniel Longacre <danieljlongacre@gmail.com>
2024-05-11 18:09:22 -0400

    Added order routes and controller skeleton

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import cors from 'cors';
 const port = process.env.PORT || 5000;
 
@@ -17,11 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-// );
-
 // Cookie parser middleware
 app.use(cookieParser());
 
@@ -31,6 +27,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
