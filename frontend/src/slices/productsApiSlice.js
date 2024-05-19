37050ba2c5d2c5f22424745ace173397565067eb
Daniel Longacre <danieljlongacre@gmail.com>
2024-05-19 08:24:19 -0400

    Added frontend and backend functionality to edit products on the admin product list screen

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { createProduct } from "../../../backend/controllers/productController";
+import { createProduct, updateProduct } from "../../../backend/controllers/productController";
 import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
@@ -8,6 +8,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             query: () => ({ 
                 url: PRODUCTS_URL,
             }),
+            providesTags: ['Product'],
             keepUnusedDataFor: 5,
         }),
         getProductDetails: builder.query({
@@ -23,11 +24,20 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
         invalidatesTags: ['Product'],
         }),
+        updateProduct: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCTS_URL}/${data.productId}`,
+                method: 'PUT',
+                body: data,
+            }),
+            invalidatesTags: ['Product'],
+        }),
     }),
 });
 
 export const { 
     useGetProductsQuery, 
     useGetProductDetailsQuery, 
-    useCreateProductMutation 
+    useCreateProductMutation,
+    useUpdateProductMutation,
 } = productsApiSlice;
