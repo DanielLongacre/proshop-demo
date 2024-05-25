6cda2d1289666d53e7d2444dfef84b720503246e
Daniel Longacre <danieljlongacre@gmail.com>
2024-05-25 17:59:59 -0400

    Added product search functionality via url keyword, without UI

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -9,9 +9,9 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
   return (
     <>
@@ -31,7 +31,7 @@ const HomeScreen = () => {
                   </Col>
               ))}
           </Row>
-          <Paginate pages={data.pages} page={data.page} />
+          <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
         </>
       )}
     </>
