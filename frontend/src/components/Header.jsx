ce296aeafa0c1ab46bdc12e274e58e7bd49ffefd
Daniel Longacre <danieljlongacre@gmail.com>
2024-05-26 19:15:37 -0400

    Small fixes to error messages and added object id middleware

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,7 @@ import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import SearchBox from './SearchBox';
 import logo from '../assets/logo.png';
+import { resetCart } from '../slices/cartSlice';
 
 const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
@@ -22,6 +23,7 @@ const Header = () => {
         try {
             await logoutApiCall().unwrap();
             dispatch(logout());
+            dispatch(resetCart());
             navigate('/login');
         } catch (err) {
             console.log(err);
@@ -31,7 +33,7 @@ const Header = () => {
 
   return (
     <header>
-        <Navbar bg="dark" variant='dark' expand='md' collapseOnSelect>
+        <Navbar bg="primary" variant='dark' expand='lg' collapseOnSelect>
             <Container>
                 <LinkContainer to='/'>
                     <Navbar.Brand>
